f47768817013391bf93538eddcee58acc46d996c
mmmmh2o <wjh765267373@gmail.com>
2026-04-25 15:46:09 +0800

    feat: add src/main/ipc/index.js

---
+++ src/main/ipc/index.js
@@ -0,0 +1,24 @@
+/**
+ * IPC Handler 统一注册
+ * 集中管理所有 IPC 通道的注册
+ */
+
+const { registerUserHandlers } = require('./userHandlers');
+const { registerSessionHandlers } = require('./sessionHandlers');
+const { registerFileHandlers } = require('./fileHandlers');
+const { registerDownloadHandlers } = require('./downloadHandlers');
+const { registerBookmarkHandlers } = require('./bookmarkHandlers');
+const { registerHistoryHandlers } = require('./historyHandlers');
+const { registerScriptHandlers } = require('./scriptHandlers');
+
+function registerAllHandlers() {
+  registerUserHandlers();
+  registerSessionHandlers();
+  registerFileHandlers();
+  registerDownloadHandlers();
+  registerBookmarkHandlers();
+  registerHistoryHandlers();
+  registerScriptHandlers();
+}
+
+module.exports = { registerAllHandlers };
